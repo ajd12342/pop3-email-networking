c9c505ff43547a33ceaf3130fad4985e35f935f8
DIWAN ANUJ JITENDRA <anujdiwan2008@gmail.com>
2019-03-10 01:49:43 +0530

    Finished till accept on server side

--- SimpleEmailServerPhase1.cpp
+++ SimpleEmailServerPhase1.cpp
@@ -7,6 +7,8 @@
 #include <sys/select.h>
 #include <fstream>
 #include <stdlib.h>
+#include <unistd.h>
+#define MAX_BACKLOG 2
 using namespace std;
 int main(int argc, char* argv[]){
 
@@ -17,8 +19,17 @@ int main(int argc, char* argv[]){
 		return 1;
 	}
 
-	//If correct usage, attempts to bind.
-	//Binds to IP address INADDR_ANY
+	//Now, checks whether passwdFile can be opened
+	string passwdFile=argv[2];
+	ifstream ifs(passwdFile,ifstream::in);
+	if(ifs.fail()){
+		cerr<<
+		"Failed to open/find file at "<<passwdFile
+		<<endl;
+		return 3;
+	}
+
+	//Bind
 	int portNum;
 	try{
 		portNum=stoi(argv[1]);
@@ -28,10 +39,9 @@ int main(int argc, char* argv[]){
 		" is not a number"<<endl;
 		return 2;
 	}
-	string passwdFile=argv[2];
 	const char* ipaddr="127.0.0.1";
 
-	int sockfd=socket(AF_INET,SOCK_STREAM,0);
+	int sockfdListen=socket(AF_INET,SOCK_STREAM,0);
 
 	struct sockaddr_in saddr;
 	saddr.sin_family=AF_INET;
@@ -39,22 +49,44 @@ int main(int argc, char* argv[]){
 	inet_aton(ipaddr,&(saddr.sin_addr));
 	for(int i=0;i<8;i++)
 		saddr.sin_zero[i]='\0';
-	int bindRet=bind(sockfd,(struct sockaddr*)&saddr,
+	int retval;
+	retval=bind(sockfdListen,(struct sockaddr*)&saddr,
 		sizeof(struct sockaddr));
 
 	//Checks for bind failure
-	if(bindRet==-1){
+	if(retval==-1){
 		cerr<<
 		"Bind failed on port "<<portNum<<endl;
 		return 2;
 	}
-	//Now, checks whether passwdFile can be opened
-	ifstream ifs(passwdFile,ifstream::in);
-	if(ifs.fail()){
+	cout<<"BindDone: "<<portNum<<endl;
+
+	//Listen
+	retval=listen(sockfdListen,MAX_BACKLOG);
+	if(retval==-1){
 		cerr<<
-		"Failed to open/find file at "<<passwdFile
-		<<endl;
-		return 3;
+		"ListenFailed: "<<portNum<<endl;
+		return 4;
 	}
-	//Everything worked till here
+	cout<<"ListenDone: "<<portNum<<endl;
+
+	//Accept
+	struct sockaddr_in caddr;
+	unsigned int addrlen=sizeof(struct sockaddr);
+	retval=accept(sockfdListen,(struct sockaddr*)&caddr,
+		&addrlen);
+	if(retval==-1){
+		cerr<<
+		"AcceptFailed"<<endl;
+		return 4;
+	}
+	int sockfd=retval;
+	int cPort=caddr.sin_port;
+	string cIP=inet_ntoa(caddr.sin_addr);
+	cout<<"Client: "<<cIP<<":"<<cPort<<endl;
+
+	
+	//Close
+	close(sockfd);
+	close(sockfdListen);
 }
